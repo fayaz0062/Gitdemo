f3079dac18a319c4c238d5e2568ed115e24fc4e1
fayaz0062 <fayaz0062@gmail.com>
2021-10-23 13:37:21 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void NativeAndroid()
+	{
+		
+		System.out.println("Android Apps");
+	}
+	
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOS Apps");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/RestAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		
+		System.out.println(" Jira Apps automation");
+	}
+	
+	//@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleting twitter App");
+	}
+}
